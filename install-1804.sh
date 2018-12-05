40a526c3b1b8c79b64365bf2a32cfab36f46b2d2
richegg <zxp86021@gmail.com>
2018-12-05 16:51:03 +0800

    update composer install section

--- install-1804.sh
+++ install-1804.sh
@@ -14,12 +14,14 @@ fi
 EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
 if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
 then
     >&2 echo 'ERROR: Invalid installer signature'
     rm composer-setup.php
     exit 1
 fi
+
 php composer-setup.php --install-dir=$HOME/bin --filename=composer
 php -r "unlink('composer-setup.php');"
 
